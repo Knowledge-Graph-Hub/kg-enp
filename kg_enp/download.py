485a2dea16eb73dd7c9af72d325c771274816b6f
caufieldjh <j.harry.caufield@gmail.com>
2024-01-16 17:56:29 -0500

    Download based on specified set of ENPKG components

--- kg_enp/download.py
+++ kg_enp/download.py
@@ -1,5 +1,12 @@
 """Download resources from YAML file."""
+
+import yaml
+
+from downloaders import BaseDownloader
+
 from kghub_downloader.download_utils import download_from_yaml  # type: ignore
+
+GRAPH_DATA_FILE = "enpkg.yaml"
 
 
 def download(
@@ -9,6 +16,9 @@ def download(
 
     DL based on config (default: download.yaml)
     into data directory (default: data/).
+
+    This also parses the file enpkg.yaml to determine additional data
+    to retrieve.
 
     :param yaml_file: A string pointing to the yaml file
     :param utilized to facilitate the downloading of data.
@@ -23,3 +33,18 @@ def download(
         snippet_only=snippet_only,
         ignore_cache=ignore_cache,
     )
+
+    with open(GRAPH_DATA_FILE, "r") as infile:
+        data = yaml.safe_load(infile)
+        datasets = [dataset for dataset in data["graphs"] if "zenodo" in dataset]
+
+    urls = []
+    paths = []
+
+    for dataset in datasets:
+        urls.append(f"https://zenodo.org/api/records/{dataset['zenodo'][0]}/files-archive")
+        paths.append(output_dir + "/" + dataset["dataset"] + ".zip")
+
+    BaseDownloader(
+        process_number=1, auto_extract=True, delete_original_after_extraction=True
+    ).download(urls=urls, paths=paths)
