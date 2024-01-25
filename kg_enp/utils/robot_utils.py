6523b373068f622955599bfa965a786e4c6f4e01
caufieldjh <j.harry.caufield@gmail.com>
2024-01-25 15:56:47 -0500

    Update ROBOT setup and utils

--- kg_enp/utils/robot_utils.py
+++ kg_enp/utils/robot_utils.py
@@ -1,129 +1,420 @@
-"""Utility to implement ROBOT over ontology files."""
+"""Utilities for working with ROBOT."""
+
 import os
-import subprocess  # noqa Source: https://docs.python.org/2/library/subprocess.html#popen-constructor
+
+import sh  # type: ignore
+from sh import chmod  # type: ignore
+
+from post_setup.post_setup import robot_setup
+
+# Note that sh module can take environment variables, see
+# https://amoffat.github.io/sh/sections/special_arguments.html#env
 
 
-def initialize_robot(path: str) -> list:
-    """
-    Initialize ROBOT with necessary configuration.
+def initialize_robot(robot_path: str) -> list:
+    """Initialize ROBOT with its necessary configuration.
 
+    During install, ROBOT is downloaded to the root project directory,
+    and the path variable used here is only necessary if it varies from
+    the project location.
     :param path: Path to ROBOT files.
-    :return: A list consisting of robot shell script name and environment variables.
+    :return: A list consisting an instance of Command and dict
+    of all environment variables.
     """
-    # Declare variables
-    robot_file = os.path.join(path, "robot")
+    # We may have made it this far without installing ROBOT, so do that now if needed
+    if not os.path.exists(robot_path):
+        robot_setup()
+
+    # Make sure it's executable
+    chmod("+x", "robot")
 
     # Declare environment variables
-    env = dict(os.environ)
-    # (JDK compatibility issue:
-    # https://stackoverflow.com/questions/49962437/unrecognized-vm-option-useparnewgc-error-could-not-create-the-java-virtual) # noqa
-    # env['ROBOT_JAVA_ARGS'] = '-Xmx8g -XX:+UseConcMarkSweepGC' # for JDK 9 and older
+    env = os.environ.copy()
     env["ROBOT_JAVA_ARGS"] = "-Xmx12g -XX:+UseG1GC"  # For JDK 10 and over
-    env["PATH"] = os.environ["PATH"]
-    env["PATH"] += os.pathsep + path
 
-    return [robot_file, env]
+    try:
+        robot_command = sh.Command(robot_path)
+    except sh.CommandNotFound:  # If for whatever reason ROBOT isn't available
+        robot_command = None
+
+    return [robot_command, env]
 
 
-def convert_to_json(path: str, ont: str):
+def relax_ontology(
+    robot_path: str, input_path: str, output_path: str, robot_env: dict
+) -> bool:
     """
-    Convert OWL to JSON using ROBOT and the subprocess library.
+    Run the ROBOT relax command on a single ontology.
 
-    :param path: Path to ROBOT and the input OWL files.
-    :param ont: Ontology
-    :return: None
+    Has a three-hour timeout limit - process is killed if it takes this long.
+    :param robot_path: Path to ROBOT files
+    :param input_owl: Ontology file to be relaxed
+    :param output_owl: Ontology file to be created (needs valid ROBOT suffix)
+    :param robot_env: dict of environment variables, including ROBOT_JAVA_ARGS
+    :return: True if completed without errors, False if errors
     """
-    robot_file, env = initialize_robot(path)
-    input_owl = os.path.join(path, ont.lower() + ".owl")
-    output_json = os.path.join(path, ont.lower() + ".json")
-    if not os.path.isfile(output_json):
-        # Setup the arguments for ROBOT through subprocess
-        call = [
-            "bash",
-            robot_file,
+    success = False
+
+    print(f"Relaxing {input_path} to {output_path}...")
+
+    robot_command = sh.Command(robot_path)
+
+    try:
+        robot_command(
+            "relax",
+            "--input",
+            input_path,
+            "--output",
+            output_path,
+            "-vvv",
+            _env=robot_env,
+            _timeout=10800,
+        )
+        print("Complete.")
+        success = True
+    except sh.ErrorReturnCode_1 as e:  # If ROBOT runs but returns an error
+        print(f"ROBOT encountered an error: {e}")
+        success = False
+
+    return success
+
+
+def robot_convert(
+    robot_path: str, input_path: str, output_path: str, robot_env: dict
+) -> bool:
+    """
+    Run a ROBOT convert command on a single ontology.
+
+    :param robot_path: Path to ROBOT files
+    :param input_path: Ontology file to be relaxed
+    :param output_path: Ontology file to be created (needs valid ROBOT suffix)
+    :param robot_env: dict of environment variables, including ROBOT_JAVA_ARGS
+    :return: True if completed without errors, False if errors
+    """
+    success = False
+
+    print(f"Converting {input_path} to {output_path}...")
+
+    robot_command = sh.Command(robot_path)
+
+    try:
+        robot_command(
             "convert",
             "--input",
-            input_owl,
+            input_path,
             "--output",
-            output_json,
-            "-f",
-            "json",
-        ]
+            output_path,
+            "-vvv",
+            _env=robot_env,
+        )
+        print("Complete.")
+        success = True
+    except sh.ErrorReturnCode_1 as e:  # If ROBOT runs but returns an error
+        print(f"ROBOT encountered an error: {e}")
+        success = False
 
-        subprocess.call(call, env=env)  # noqa
-
-    return None
+    return success
 
 
-def extract_convert_to_json(path: str, ont_name: str, terms: str, mode: str):
+def merge_and_convert_ontology(
+    robot_path: str, input_path: str, output_path: str, robot_env: dict
+) -> bool:
     """
-    Extract all children of provided CURIE.
+    Run a merge and convert ROBOT command on a single ontology.
 
-    ROBOT Method options:
-
-    -   STAR: The STAR-module contains mainly the terms in the seed and the
-    inter-relations between them (not necessarily sub- and super-classes).
-
-    -   TOP: The TOP-module contains mainly the terms in the seed, plus all
-    their sub-classes and the inter-relations between them.
-
-    -   BOT: The BOT, or BOTTOM, -module contains mainly the terms in the seed,
-    plus all their super-classes and the inter-relations between them.
-
-    -   MIREOT : The MIREOT method preserves the hierarchy of the input ontology
-    (subclass and subproperty relationships), but does not try to preserve the
-    full set of logical entailments.
-
-    :param path: path of file to be converted
-    :param ont_name: Name of the ontology
-    :param terms: Either CURIE or a file of CURIEs list
-    :param mode: Method options as listed below.
-    :return: None
+    Has a three-hour timeout limit - process is killed if it takes this long.
+    :param robot_path: Path to ROBOT files
+    :param input_path: Ontology file to be relaxed
+    :param output_path: Ontology file to be created (needs valid ROBOT suffix)
+    :param robot_env: dict of environment variables, including ROBOT_JAVA_ARGS
+    :return: True if completed without errors, False if errors
     """
-    robot_file, env = initialize_robot(path)
-    input_owl = os.path.join(path, ont_name.lower() + ".owl")
-    output_json = os.path.join(path, ont_name.lower() + ".json")
-    output_owl = os.path.join(path, ont_name.lower() + "_extracted_subset.owl")
+    success = False
 
-    if ":" in terms:
-        call = [
-            "bash",
-            robot_file,
-            "extract",
-            "--method",
-            mode,
+    print(f"Merging and converting {input_path} to {output_path}...")
+
+    robot_command = sh.Command(robot_path)
+
+    try:
+        robot_command(
+            "merge",
             "--input",
-            input_owl,
+            input_path,
+            "convert",
             "--output",
-            output_owl,
+            output_path,
+            "-vvv",
+            _env=robot_env,
+            _timeout=10800,
+        )
+        print("Complete.")
+        success = True
+    except sh.ErrorReturnCode_1 as e:  # If ROBOT runs but returns an error
+        print(f"ROBOT encountered an error: {e}")
+        success = False
+
+    return success
+
+
+def measure_ontology(
+    robot_path: str, input_path: str, output_log: str, robot_env: dict
+) -> bool:
+    """
+    Run the ROBOT measure command on a single ontology.
+
+    Yields all metrics as string and as a log file.
+    :param robot_path: Path to ROBOT files
+    :param input_owl: Ontology file to be validated
+    :param output_owl: Location of log file to be created
+    :param robot_env: dict of environment variables, including ROBOT_JAVA_ARGS
+    :return: True if completed without errors, False if errors
+    """
+    success = False
+
+    print(f"Obtaining metrics for {input_path}...")
+
+    robot_command = sh.Command(robot_path)
+
+    try:
+        robot_command(
+            "measure",
+            "--input",
+            input_path,
+            "--format",
+            "tsv",
+            "--metrics",
+            "all",
+            "--output",
+            output_log,
+            _env=robot_env,
+        )
+        print(f"Complete. See log in {output_log}")
+        success = True
+    except sh.ErrorReturnCode_1 as e:  # If ROBOT runs but returns an error
+        print(f"ROBOT encountered an error: {e}")
+        success = False
+
+    return success
+
+
+def robot_remove(
+    robot_path: str, input_path: str, output_path: str, term: str, robot_env: dict
+) -> bool:
+    """
+    Run the ROBOT remove command on a single ontology.
+
+    :param robot_path: Path to ROBOT files
+    :param input_path: Ontology file for input
+    :param output_path: Ontology file to be created (needs valid ROBOT suffix)
+    :param term: term to select for removal
+    :param robot_env: dict of environment variables, including ROBOT_JAVA_ARGS
+    :return: True if completed without errors, False if errors
+    """
+    success = False
+
+    print(f"Removing selected elements from {input_path}: {term}...")
+
+    robot_command = sh.Command(robot_path)
+
+    try:
+        robot_command(
+            "remove",
+            "-vvv",
+            "--input",
+            input_path,
             "--term",
-            terms,
-            "convert",
+            term,
             "--output",
-            output_json,
-            "-f",
-            "json",
-        ]
-    else:
-        call = [
-            "bash",
-            robot_file,
-            "extract",
-            "--method",
-            mode,
+            output_path,
+            _env=robot_env,
+        )
+        print(f"Complete. See {output_path}")
+        success = True
+    except sh.ErrorReturnCode_1 as e:  # If ROBOT runs but returns an error
+        print(f"ROBOT encountered an error: {e}")
+        success = False
+
+    return success
+
+
+def robot_report(
+    robot_path: str, input_path: str, output_path: str, robot_env: dict
+) -> bool:
+    """
+    Run the ROBOT report command on a single ontology.
+
+    :param robot_path: Path to ROBOT files
+    :param input_path: Ontology file for input
+    :param output_path: Path to create report at
+    :param robot_env: dict of environment variables, including ROBOT_JAVA_ARGS
+    :return: True if completed without errors, False if errors
+    """
+    success = False
+
+    print(f"Generating ROBOT report for {input_path}...")
+
+    robot_command = sh.Command(robot_path)
+
+    try:
+        robot_command(
+            "report",
             "--input",
-            input_owl,
+            input_path,
             "--output",
-            output_owl,
-            "--term-file",
-            terms,
-            "convert",
+            output_path,
+            "--format",
+            "tsv",
+            _env=robot_env,
+        )
+        print(f"No errors here! See {output_path}")
+        success = True
+    except sh.ErrorReturnCode_1 as e:  # If ROBOT runs but returns an error
+        # For report, this is expected, as the error may be
+        # in the target ontology.
+        print(f"ROBOT report results: {e}\nSee {output_path}")
+        success = False
+
+    return success
+
+
+def robot_measure(
+    robot_path: str, input_path: str, output_path: str, robot_env: dict
+) -> bool:
+    """
+    Run the ROBOT measure command on a single ontology.
+
+    Return all metrics.
+    :param robot_path: Path to ROBOT files
+    :param input_path: Ontology file for input
+    :param output_path: Path to create measure log at
+    :param robot_env: dict of environment variables, including ROBOT_JAVA_ARGS
+    :return: True if completed without errors, False if errors
+    """
+    success = False
+
+    print(f"Generating ROBOT measure log for {input_path}...")
+
+    robot_command = sh.Command(robot_path)
+
+    try:
+        robot_command(
+            "measure",
+            "-vvv",
+            "--input",
+            input_path,
             "--output",
-            output_json,
-            "-f",
-            "json",
-        ]
+            output_path,
+            "--format",
+            "tsv",
+            "--metrics",
+            "all",
+            _env=robot_env,
+        )
+        print(f"Complete. See {output_path}")
+        success = True
+    except sh.ErrorReturnCode_1 as e:  # If ROBOT runs but returns an error
+        print(f"ROBOT encountered an error: {e}")
+        success = False
 
-    subprocess.call(call, env=env)  # noqa
+    return success
 
-    return None
+
+def robot_query_construct(
+    robot_path: str, input_path: str, query_path: str, output_path: str, robot_env: dict
+) -> bool:
+    """
+    Run the ROBOT query command on a single ontology.
+
+    Assumes that the query is a CONSTRUCT.
+    This means the output will be RDF.
+    :param robot_path: Path to ROBOT files
+    :param input_path: Ontology file for input
+    :param query_path: Path to file containing SPARQL CONSTRUCT query
+    :param output_path: Path to create output file.
+                        Robot defaults to Turtle.
+    :param robot_env: dict of environment variables, including ROBOT_JAVA_ARGS
+    :return: True if completed without errors, False if errors
+    """
+    success = False
+
+    print(f"Running CONSTRUCT query from {query_path} on {input_path}...")
+
+    robot_command = sh.Command(robot_path)
+
+    try:
+        robot_command(
+            "query",
+            "-vvv",
+            "--input",
+            input_path,
+            "--format",
+            "owl",
+            "--query",
+            query_path,
+            output_path,
+            _env=robot_env,
+        )
+        print(f"Complete. See {output_path}")
+        success = True
+    except sh.ErrorReturnCode_1 as e:  # If ROBOT runs but returns an error
+        print(f"ROBOT encountered an error: {e}")
+        success = False
+
+    return success
+
+
+def robot_query_update(
+    robot_path: str,
+    input_path: str,
+    query_path: str,
+    output_path: str,
+    use_temp_file: bool,
+    robot_env: dict,
+) -> bool:
+    """
+    Run the ROBOT query command on a single ontology.
+
+    Assumes that the query is an Update.
+    :param robot_path: Path to ROBOT files
+    :param input_path: Ontology file for input (i.e., to be updated)
+    :param query_path: Path to file containing SPARQL Update query
+    :param output_path: Path to create output file.
+    :param use_temp_file: bool, if True, save intermediate results to a temporary file
+    to save memory
+    :param robot_env: dict of environment variables, including ROBOT_JAVA_ARGS
+    :return: True if completed without errors, False if errors
+    """
+    success = False
+
+    print(f"Running Update query from {query_path} on {input_path}...")
+
+    robot_command = sh.Command(robot_path)
+
+    if use_temp_file:
+        temp_file_param = "true"
+    else:
+        temp_file_param = "false"
+
+    try:
+        robot_command(
+            "query",
+            "-vvv",
+            "--input",
+            input_path,
+            "--format",
+            "owl",
+            "--update",
+            query_path,
+            "--temporary-file",
+            temp_file_param,
+            "--output",
+            output_path,
+            _env=robot_env,
+        )
+        print(f"Complete. See {output_path}")
+        success = True
+    except sh.ErrorReturnCode_1 as e:  # If ROBOT runs but returns an error
+        print(f"ROBOT encountered an error: {e}")
+        success = False
+
+    return success
